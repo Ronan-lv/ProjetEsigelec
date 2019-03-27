e6a37dca2989152748f5559e5e832243bdeeb3bb
Ronan <rlv78@hotmail.fr>
2019-03-27 16:07:56 +0100

    Remplissage de la classe fenêtre

--- ProjetEsigelec/src/Ihm/Fenetre.java
+++ ProjetEsigelec/src/Ihm/Fenetre.java
@@ -1,5 +1,25 @@
 package Ihm;
 
-public class Fenetre {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Fenetre extends JFrame{
+	
+	private String titreFenetre = "GÏT";
+	private Dimension dimensionFenetre = new Dimension(750,600);
+	private JPanel lePanel;
+	
+	public Fenetre(){
+		this.setTitle(titreFenetre);
+		this.setSize(dimensionFenetre);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setContentPane(lePanel);
+		
+		this.setVisible(true);
+	}
+		
 }
