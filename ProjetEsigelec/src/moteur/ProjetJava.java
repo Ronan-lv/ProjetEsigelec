bfbf455b5a8099ea58fc465f4117c1abf68ef34e
gael-leroux <gael.leroux@hotmail.fr>
2019-04-01 22:48:51 +0200

    Création de la Classe Utilisateur et ProjetJava
    remplissage des classe utilisateur et projet java les liens ne sont pas encore réalisés

--- ProjetEsigelec/src/moteur/ProjetJava.java
+++ ProjetEsigelec/src/moteur/ProjetJava.java
@@ -1,5 +1,59 @@
 package moteur;
-
+/**
+ * 
+ * @author Gael Le Roux et Ronan Le Viennesse
+ * version :
+ * La classe ProjetJava du logiciel 
+ */
 public class ProjetJava {
-
-}
+// déclaration des variables de la classe ProjetJava
+	private String nom_projet ;
+	private String destination_projet ;
+	// (rajouter liste version ,date_projet  )
+	
+	// constructeur 
+	/**
+	 * Constructeur de la classe ProjetJava
+	 * @param nom_projet, destination_projet
+	 */
+	public ProjetJava (String nom ,String destination) {
+		this.nom_projet =nom ;
+		this.destination_projet =destination ;
+	}
+	// les getters de la classe ProjetJava
+	/**
+	 * getter nom_projet
+	 * permet de récupérer le nom du projet
+	 * @return nom_projet
+	 */
+	public String GetNomProjet ()
+	{
+		return nom_projet ;
+	}
+	/**
+	 * getter destination_projet
+	 * permet de récupérer la destination du projet
+	 * @return destination_projet
+	 */
+	public String GetDestinationProjet ()
+	{
+		return destination_projet ;
+	}
+	// les setters de la classe ProjetJava
+	/**
+	 * setter nom_projet
+	 * permet de remplacer ou saisir le nom du projet
+	 * @param nom_projet
+	 */
+	public void SetNomProjet(String nom) {
+		this.nom_projet = nom ;
+	}
+	/**
+	 * setter destination_projet
+	 * permet de remplacer ou saisir la destination du projet
+	 * @param destination_projet
+	 */
+	public void SetDestinationProjet(String destination) {
+		this.destination_projet = destination ;
+	}
+	}
--- ProjetEsigelec/src/moteur/Utilisateur.java
+++ ProjetEsigelec/src/moteur/Utilisateur.java
@@ -1,5 +1,85 @@
 package moteur;
 
+/**
+ * 
+ * @author Gael Leroux et Ronan Le Viennesse version : La classe utilisateur du
+ *         logiciel
+ */
 public class Utilisateur {
 
+// déclarations des variables de la classe Utilisateur  :	
+	private String identifiant;
+	private String mot_de_passe;
+	private boolean isGestionnaire;
+//(rajouter liste projet et historique )
+	
+	// le constructeur de Utilisateur
+	/**
+	 * Constructeur de la classe Utilisateur
+	 * 
+	 * @param identifiaznt , mot_de_passe ,type
+	 */
+	public Utilisateur(String id, String mdp, boolean g) {
+		this.identifiant = id;
+		this.mot_de_passe = mdp;
+		this.isGestionnaire = g;
+	}
+
+// les getters de Utilisateur :	
+	/**
+	 * getter identifiant permet de récupérer l'identifiaznt d'un utilisateur
+	 * 
+	 * @return identifiant
+	 */
+	public String GetIdentifiant() {
+		return identifiant;
+	}
+
+	/**
+	 * getter mot_de_passe permet de récupérer le mot_de_passe d'un utilisateur
+	 * 
+	 * @return mot_de_passe
+	 */
+	public String GetMotDePasse() {
+		return mot_de_passe;
+	}
+
+	/**
+	 * getter isGestionnaire permet de récupérer le type d'utilisation
+	 * 
+	 * @return isGestionnaire
+	 */
+	public boolean GetIsGestionnaire() {
+		return isGestionnaire;
+	}
+
+// les setters de Utilisateur 
+	/**
+	 * setter identifiant permet de remplacer ou saisir l'identifiant d'un
+	 * utilisateur
+	 * 
+	 * @param identifiant
+	 */
+	public void SetIdentifiant(String id) {
+		this.identifiant = id;
+	}
+
+	/**
+	 * setter mot_de_passe permet de remplacer ou saisir le mot_de_passe d'un
+	 * utilisateur
+	 * 
+	 * @param mot_de_passe
+	 */
+	public void SetMotDePasse(String mdp) {
+		this.mot_de_passe = mdp;
+	}
+
+	/**
+	 * setter isGestionnaire permet de remplacer ou saisir le type d'utilisation
+	 * 
+	 * @param isGestionnaire
+	 */
+	public void SetIsGestionnaire(boolean g) {
+		this.isGestionnaire = g;
+	}
 }
