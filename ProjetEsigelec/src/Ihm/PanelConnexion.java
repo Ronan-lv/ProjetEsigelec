6d841a50571a6ebd4e93b1223bf9fea5a97b7f77
Ronan <rlv78@hotmail.fr>
2019-04-03 18:16:06 +0200

    Création de la classe/IHM connexion
    Création du premier panel IHM, sans gestion des évènements

--- ProjetEsigelec/src/Ihm/PanelConnexion.java
+++ ProjetEsigelec/src/Ihm/PanelConnexion.java
@@ -1,7 +1,12 @@
 package Ihm;
 
-import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
@@ -11,17 +16,40 @@ public class PanelConnexion extends PanelGenerique {
 		private JTextField champLogin;
 		private JTextField champPassword;
 		
-		private Button bouttonValider;
+		private JButton bouttonValider;
+		
+		
 		
 		public PanelConnexion() {
-
-			this.texteTitre = new JLabel("Salut");
+			
+			this.texteTitre = new JLabel("Identification");
+			this.texteTitre.setFont(policeTaille2);
+			this.texteTitre.setHorizontalAlignment(this.texteTitre.CENTER);
+			
 			this.champLogin = new JTextField("Login");
+			this.champLogin.setFont(policeTaille2);
+			
 			this.champPassword = new JTextField("Password");
+			this.champPassword.setFont(policeTaille2);
+			
+			this.bouttonValider = new JButton("Valider");
+			this.bouttonValider.setFont(policeTaille2);
+			
+			
+			this.setLayout(new GridLayout(5,0,0,20)); //Les 2 dernières valeurs correspondent respectivement à l'espacement entre les lignes et les colonnes
+			
+			
+			this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(120, 200, 120, 200), //Empty border => top, left, bottom, right
+															  BorderFactory.createCompoundBorder(
+																	  BorderFactory.createLineBorder(Color.BLACK, 3), 
+																	  BorderFactory.createEmptyBorder(20, 20, 20, 20)
+																	  )));
+					
 			
 			this.add(texteTitre);
 			this.add(champLogin);
 			this.add(champPassword);
+			this.add(bouttonValider);
 			
 		}
 }
--- ProjetEsigelec/src/Ihm/PanelGenerique.java
+++ ProjetEsigelec/src/Ihm/PanelGenerique.java
@@ -1,6 +1,7 @@
 package Ihm;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JPanel;
 
@@ -9,8 +10,12 @@ public class PanelGenerique extends JPanel{
 	protected final Color couleurDeFond = new Color(245, 245, 245); //blanc
 	protected final Color couleurPrimaire = new Color(234, 235, 233); 
 	protected final Color couleurSecondaire = new Color(234, 235, 233);
+	
+	protected final Font policeTaille1 = new Font("Arial", Font.PLAIN, 15);
+	protected final Font policeTaille2 = new Font("Arial", Font.BOLD, 20);
 		
 	public PanelGenerique() {
 		this.setBackground(couleurDeFond);
+	
 	}
 }
