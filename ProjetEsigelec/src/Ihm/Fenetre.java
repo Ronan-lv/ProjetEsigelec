c7b45140049cc2d59275862fa57a799cb10eafaa
Ronan <rlv78@hotmail.fr>
2019-04-28 22:36:01 +0200

    Ajouts fonctionnels
    (commit qui aurait du être ajouté en amont)

--- ProjetEsigelec/src/Ihm/Fenetre.java
+++ ProjetEsigelec/src/Ihm/Fenetre.java
@@ -5,6 +5,11 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import DAO.DAOProjetJava;
+import DAO.DAOUtilisateur;
+import DAO.DAOVersionFichier;
+import moteur.Utilisateur;
+
 public class Fenetre extends JFrame{
 	
 	/**
@@ -15,6 +20,15 @@ public class Fenetre extends JFrame{
 	private Dimension dimensionFenetre = new Dimension(750,600);
 	private JPanel lePanel = new PanelConnexion(this);
 	
+	private DAOUtilisateur daoUtilisateur;
+	private DAOProjetJava daoProjetJava;
+	private DAOVersionFichier daoVersionFichier;
+	
+	private Utilisateur UtilisateurActif;
+	
+	private String stringDeTest;
+	
+
 	public Fenetre(){
 		
 		this.setTitle(titreFenetre);
@@ -25,7 +39,75 @@ public class Fenetre extends JFrame{
 		this.setContentPane(lePanel);
 		
 		this.setVisible(true);
+		
+		/* On commence à charger les données depuis la BDD uniquement après avoir afficher la fenêtre.
+		 * Cela permet non seulement d'éviter à l'utilisateur de devoir patienter sans rien affiché à l'écran, 
+		 * mais également cela laisse la possibilité dans un développement futur, d'implémenter une interface graphique
+		 * destinée à le faire patentier durant le chargement. 
+		 */
+		this.daoUtilisateur = new DAOUtilisateur();
+		this.daoProjetJava = new DAOProjetJava();
+		this.daoVersionFichier = new DAOVersionFichier();
 	}
+	
+	
+	
+	public DAOUtilisateur getDaoUtilisateur() {
+		return daoUtilisateur;
+	}
+
+	public void setDaoUtilisateur(DAOUtilisateur daoUtilisateur) {
+		this.daoUtilisateur = daoUtilisateur;
+	}
+	
+	
+	public DAOProjetJava getDaoProjetJava() {
+		return daoProjetJava;
+	}
+
+	public void setDaoProjetJava(DAOProjetJava daoProjetJava) {
+		this.daoProjetJava = daoProjetJava;
+	}
+	
+
+	public DAOVersionFichier getDaoVersionFichier() {
+		return daoVersionFichier;
+	}
+
+
+	public void setDaoVersionFichier(DAOVersionFichier daoVersionFichier) {
+		this.daoVersionFichier = daoVersionFichier;
+	}
+
+
+
+	
+	
+	
+	
+	public Utilisateur getUtilisateurActif() {
+		return UtilisateurActif;
+	}
+
+	public void setUtilisateurActif(Utilisateur utilisateurActif) {
+		UtilisateurActif = utilisateurActif;
+	}
+
+	
+	
+	
+	//test
+	public String getStringDeTest() {
+		return stringDeTest;
+	}
+	public void setStringDeTest(String stringDeTest) {
+		this.stringDeTest = stringDeTest;
+	}
+	
+	
+	//test
+	
+	
 	
 
 		
